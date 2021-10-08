e46e44cfe3cdcf74c6b3585e5e04bc4058013e99
lbideau <leshliebzt@gmail.com>
2021-10-08 10:47:02 -0400

    New Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(randomSuits());
+  document.querySelector("span").innerHTML = randomNumbers();
 };
+
+function randomSuits() {
+  let pinta = ["spade", "heart", "diamand", "club"];
+  let pintaPosition = Math.floor(Math.random() * pinta.length);
+  return pinta[pintaPosition];
+}
+function randomNumbers() {
+  let card = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardPosition = Math.floor(Math.random() * card.length);
+  return card[cardPosition];
+}
